198daa91bf88f25a3a46c46600d93883d954e868
Emma Siraudeau <emma.siraudeau@efpedu.fr>
2025-10-22 12:47:07 +0200

    création d'un nouveau projet TP0 calculette

---
+++ TP0_calculette/src/tp0_calculette/TP0_calculette.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp0_calculette;
+
+/**
+ *
+ * @author emmas
+ */
+public class TP0_calculette {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("""
+                        1) add 
+                        2) substract 
+                        3) multiply 
+                        4) divide 
+                        5) modulo """);
+    }
+    
+}
+  
