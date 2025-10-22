6078afd6e9582a9f7e7f9321c026617b0dfc406e
Emma Siraudeau <emma.siraudeau@efpedu.fr>
2025-10-22 12:46:33 +0200

    création d'un nouveau projet TP1

---
+++ TP1_manipNombresInt_SIRAUDEAU/src/tp1_manipnombresint_siraudeau/TP1_manipNombresInt_SIRAUDEAU.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_manipnombresint_siraudeau;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author emmas
+ */
+public class TP1_manipNombresInt_SIRAUDEAU {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    // TODO code application logic here
+        int nombre1;
+        int nombre2;
+        int result;
+        Scanner sc = new Scanner(System.in); 
+        System.out.print("Entrez le premier entier : ");
+        nombre1 = sc.nextInt();
+        System.out.print("Entrez le deuxieme entier : ");
+        nombre2 = sc.nextInt();
+        result=0
+        {  
+        result=result+nombre1+nombre2; 
+        }  
+        // affichage du resultat  
+        System.out.println("la somme de "+ nombre1 + "et " + nombre2 "est :"+result); 
+        
+    }
+    
+}
