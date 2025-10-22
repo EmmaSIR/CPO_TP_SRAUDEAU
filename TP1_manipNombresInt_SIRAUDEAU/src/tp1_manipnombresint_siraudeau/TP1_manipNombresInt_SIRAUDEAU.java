37185eadd892b2a148715949757de0c2538c8441
Emma Siraudeau <emma.siraudeau@efpedu.fr>
2025-10-22 12:56:40 +0200

    création d'un nouveau projet TP1

--- TP1_manipNombresInt_SIRAUDEAU/src/tp1_manipnombresint_siraudeau/TP1_manipNombresInt_SIRAUDEAU.java
+++ TP1_manipNombresInt_SIRAUDEAU/src/tp1_manipnombresint_siraudeau/TP1_manipNombresInt_SIRAUDEAU.java
@@ -10,28 +10,35 @@ import java.util.Scanner;
  *
  * @author emmas
  */
-public class TP1_manipNombresInt_SIRAUDEAU {
+public class TP1_manipNombresInt_SIRAUDEAU 
+{
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
     // TODO code application logic here
         int nombre1;
         int nombre2;
-        int result;
+        int somme;
+        int diff; 
         Scanner sc = new Scanner(System.in); 
         System.out.print("Entrez le premier entier : ");
         nombre1 = sc.nextInt();
         System.out.print("Entrez le deuxieme entier : ");
         nombre2 = sc.nextInt();
-        result=0
+        somme=0;
+        diff=0;
         {  
-        result=result+nombre1+nombre2; 
+        somme=somme+nombre1+nombre2; 
         }  
         // affichage du resultat  
-        System.out.println("la somme de "+ nombre1 + "et " + nombre2 "est :"+result); 
-        
+        System.out.println("la somme de "+ nombre1 + " et " + nombre2+ " est :"+somme); 
+        {
+         diff=diff+nombre1-nombre2;
+        }
+        System.out.println("la difference de "+ nombre1 + " et " + nombre2+ " est :"+diff);
     }
     
 }
