449320a2bd0aef520b856dffe4937f44dae495df
Emma Siraudeau <emma.siraudeau@efpedu.fr>
2025-10-24 12:58:45 +0200

    création d'un nouveau projet TP1_convertisseur

---
+++ TP1_convertisseur_SIRAUDEAU/src/tp1_convertisseur_siraudeau/TP1_convertisseur_SIRAUDEAU.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_convertisseur_siraudeau;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author emmas
+ */
+public class TP1_convertisseur_SIRAUDEAU {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        double nombre1;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Entrez une valeure réelle : ");
+        nombre1=as.nextDouble();
+    }
+    
+}
