75ba91b8e682392e3e40516f1a28c4b4a21a8dd2
Emma Siraudeau <emma.siraudeau@efpedu.fr>
2025-11-04 12:59:50 +0100

    création d'un nouveau projet TP1_stats

---
+++ TP2_Bieres_SIRAUDEAU/src/tp2_bieres_siraudeau/BouteilleBiere.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_bieres_siraudeau;
+
+/**
+ *
+ * @author emmas
+ */
+public class BouteilleBiere 
+{
+    String Nom;
+    double degreAlcool;
+    String brasserie;
+    boolean ouverte;
+    
+    public BouteilleBiere(String unNom, double unDegre, String uneBrasserie) 
+    { 
+        Nom = unNom; 
+        degreAlcool = unDegre; 
+        brasserie = uneBrasserie; 
+        ouverte = false; 
+    } 
+
+    public void lireEtiquette() 
+    { 
+    System.out.println("Bouteille de " + Nom +" (" + degreAlcool + 
+    " degres) \nBrasserie : " + brasserie ) ; 
+    } 
+    
+    public boolean Décapsuler() 
+    {
+        if (ouverte==false)
+        {
+            ouverte=true;
+            return true;
+        }
+        else 
+        {
+            System.out.println("erreur : biere déjà ouverte");
+            return false;
+        }
+    } 
+    
+    public boolean toString() 
+    { 
+        
+    }
+
+    
+}
+
