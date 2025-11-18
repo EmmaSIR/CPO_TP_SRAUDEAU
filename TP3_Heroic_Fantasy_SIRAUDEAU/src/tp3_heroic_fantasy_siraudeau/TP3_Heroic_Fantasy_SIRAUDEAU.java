e5fea34425f8a6b8bc29edf3f673f30daf45d682
Emma Siraudeau <emma.siraudeau@efpedu.fr>
2025-11-18 12:48:55 +0100

    création d'un nouveau projet TP2_Heroic_SIRAUDEAU

--- TP3_Heroic_Fantasy_SIRAUDEAU/src/tp3_heroic_fantasy_siraudeau/TP3_Heroic_Fantasy_SIRAUDEAU.java
+++ TP3_Heroic_Fantasy_SIRAUDEAU/src/tp3_heroic_fantasy_siraudeau/TP3_Heroic_Fantasy_SIRAUDEAU.java
@@ -4,20 +4,62 @@
  */
 package tp3_heroic_fantasy_siraudeau;
 
+import Armes.Epee;
+import Armes.Arme;
+import Armes.Baton;
+import java.util.ArrayList;
+import java.util.Vector;
+
 /**
  *
  * @author emmas
  */
-public class TP3_Heroic_Fantasy_SIRAUDEAU {
 
+
+public class TP3_Heroic_Fantasy_SIRAUDEAU {
+    
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    
+    public static void main(String[] args) 
+    {
         // TODO code application logic here
         
+        Epee excalibur = new Epee("Excalibur", 7, 5);
+        Epee durandal = new Epee("Durandal", 4, 7);
         
+        Baton chene = new Baton("Chêne", 4, 5);
+        Baton charme = new Baton("Charme", 5, 6);
         
+        System.out.println(excalibur);
+        System.out.println(durandal);
+        System.out.println(chene);
+        System.out.println(charme);
+        
+        ArrayList<Arme> listeArmes = new ArrayList<>();
+
+        listeArmes.add(excalibur);
+        listeArmes.add(durandal);
+        listeArmes.add(chene);
+        listeArmes.add(charme);
+        
+        System.out.println("\n--- Affichage avec ArrayList ---");
+        for (Arme a : listeArmes) {
+            System.out.println(a);
+        }
+
+        Vector<Arme> listeArmesVect = new Vector<>();
+
+        listeArmes.add(excalibur);
+        listeArmes.add(durandal);
+        listeArmes.add(chene);
+        listeArmes.add(charme);
+
+         System.out.println("\n--- Affichage avec Vector (elementAt) ---");
+        for (int i = 0; i < listeArmesVect.size(); i++) {
+            System.out.println(listeArmesVect.elementAt(i));
+        }
     }
     
 }
